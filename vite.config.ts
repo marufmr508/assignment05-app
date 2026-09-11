39b9e4733e4af001ee3bc61014ff8611450fe05c
AL MARUF HASAN <marufmd01708@gmail.com>
2026-09-11 15:51:21 +0600

    ggg

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+ 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-   base:'/assignment05-app/',
+ 
 })
